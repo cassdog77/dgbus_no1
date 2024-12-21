608ef5387849ed8d99dab6856f02bf8eeaae92a8
cassdog77 <cassdog77@gmail.com>
2024-12-21 22:19:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
-FILTER_TIME = 10
+FILTER_TIME = 30
 
 def get_bus_data(stop_id):
     url = api_url_template.format(stop_id)
