32e596c71e1856da9452022b339e53126e7d9ecb
cassdog77 <cassdog77@gmail.com>
2025-01-06 23:26:52 +0900

    250106

--- app.py
+++ app.py
@@ -10,14 +10,18 @@ bus_stops_am = {
         "name": "메트로팔레스1",
         "filters": ["425","937"]
     },
+    "7011002200": {
+        "name": "신천LH",
+        "filters": ["북구3","101"]
+    },
     "7011006700": {
         "name": "동대구역",
         "filters": ["708"]
     },
-    "7011003900": {
-        "name": "아이위시앞",
-        "filters": ["북구3"]
-    }
+    "7001005900": {
+        "name": "섬유회관건너",
+        "filters": ["동구2","939"]
+    } 
 }
 
 bus_stops_pm = {
@@ -25,15 +29,11 @@ bus_stops_pm = {
         "name": "고성지구대",
         "filters": ["708"]
     },
-    "7021013400": {
-        "name": "북구청건너",
-        "filters": ["708"]
-    },
     "7021013700": {
         "name": "kt북대구건너",
         "filters": ["234"]
     },      
-    "7061038700": {
+    "7021030500": {
         "name": "고성아파트건너",
         "filters": ["북구3"]
     },
@@ -44,8 +44,12 @@ bus_stops_pm = {
 }
 
 bus_id = {
-    "북구3": "4060003000",
-    "425": "3000425000",
+    "북구3": ["4060003000"],
+    "425": ["3000425000"],
+    "101": ["3000101000"],
+    "234": ["3000234000"],
+    "708": ["3000708000","3000708001"]
+    
 }
 
 station_name_map = {}
@@ -57,8 +61,7 @@ headers = {
 
 # 버스 도착 정보 가져오기
 def get_bus_data(stop_id):
-    # API URL 
-    url = "https://businfo.daegu.go.kr:8095/dbms_web_api/realtime/arr/{}?_={}".format(stop_id,str(int(time.time() * 1000)))
+    url = "https://businfo.daegu.go.kr:8095/dbms_web_api/realtime/arr/{}?_={}".format(stop_id, str(int(time.time() * 1000)))
     response = requests.get(url, headers=headers)
     data = response.json()
     buses = []
@@ -92,54 +95,57 @@ def process_bus_data(bus_stops):
         all_buses.extend(filtered_buses)
     return all_buses
 
-
 # 버스번호 -> 위치 정보 가져오기
-def get_bus_location(bus,dir):
-    # 정류장
-    url = "https://businfo.daegu.go.kr:8095/dbms_web_api/bs/route?routeId={}".format(bus_id[bus])
-    response = requests.get(url, headers=headers)
-    data = response.json()
+def get_bus_location(bus, dir):
+    bus_routes = bus_id[bus]
     bus_stops = []
-    for i in data.get('body', []):
-        bs_id = i.get("bsId")
-        bs_name = i.get("bsNm")
-        station_name_map[bs_id] = bs_name
-        bus_stop = {
-            "lat": i.get("lat"),
-            "lng": i.get("lng"),
-            "stationName": bs_name
-        }
-        bus_stops.append(bus_stop)
-    
-    # 버스위치
-    url = "https://businfo.daegu.go.kr:8095/dbms_web_api/bs/position?routeId={}".format(bus_id[bus])
-    #print(url)
-    response = requests.get(url, headers=headers)
-    data = response.json()
     bus_locations = []
-    for i in data.get('body', []):
-        if i.get("moveDir") == dir:
-            bus_location = {
-                "moveDir": i.get("moveDir"), 
+    
+    # 각 버스에 대해 정류장과 버스 위치 정보 가져오기
+    for route in bus_routes:
+        # 정류장 정보
+        url = "https://businfo.daegu.go.kr:8095/dbms_web_api/bs/route?routeId={}".format(route)
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        for i in data.get('body', []):
+            bs_id = i.get("bsId")
+            bs_name = i.get("bsNm")
+            station_name_map[bs_id] = bs_name
+            bus_stop = {
                 "lat": i.get("lat"),
                 "lng": i.get("lng"),
-                "stationName": station_name_map[i.get("bsId")] 
+                "stationName": bs_name
             }
-            bus_locations.append(bus_location)
+            bus_stops.append(bus_stop)
+        
+        # 버스 위치 정보
+        url = "https://businfo.daegu.go.kr:8095/dbms_web_api/bs/position?routeId={}".format(route)
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        for i in data.get('body', []):
+            if i.get("moveDir") == dir:
+                bus_location = {
+                    "moveDir": i.get("moveDir"),
+                    "lat": i.get("lat"),
+                    "lng": i.get("lng"),
+                    "stationName": station_name_map[i.get("bsId")]
+                }
+                bus_locations.append(bus_location)
+    
     return bus_stops, bus_locations
 
 # Flask 라우트
 @app.route('/bus', methods=['GET'])
 def bus_route():
-    item = request.args.get('item', 'am') 
+    item = request.args.get('item', 'am')
     if item == 'am':
         bus_stops = bus_stops_am
     elif item == 'pm':
         bus_stops = bus_stops_pm
     else:
-        bus = item 
-        dir = request.args.get('dir', '1') 
-        bus_stops, bus_locations = get_bus_location(bus,dir)
+        bus = item
+        dir = request.args.get('dir', '1')
+        bus_stops, bus_locations = get_bus_location(bus, dir)
         return render_template('bus_location.html', bus_locations=bus_locations, bus_stops=bus_stops, 
                                item=item, api_key=os.getenv('KAKAO_MAPS_API_KEY'))
 
@@ -148,4 +154,3 @@ def bus_route():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
